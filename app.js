00c5c9dd53484ef199cc2d0510459decf1241b4d
Cledb Astudillo <cledb.uxcodes@gmail.com>
2021-12-15 06:34:28 +0000

    added line 9 and 10 that was initially removed

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const express = require('express');
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views')); // After setting the engine below, we need to set another set method and passing the 'views' setting to let express know where it will find our template files that we want our templating engine to process. Then we enter a second value which is the value for the 'views' option which is a path to the folder that contains the template files.
+app.set('view engine', 'ejs'); // using the set method will allow us to set certain options in this express app. In this line we're using 2 options: 1) 'view engine' which will tell express that we want to use a template engine for processing our view files before we sent them back as an html. 2) 'ejs' as this is what we installed and what template engine we planned to use.
+
 app.use(express.static('public'));
 app.use(express.urlencoded({
     extended: false
