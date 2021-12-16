e5f68f800c776fb464a0f6f47f1ec06b21b9b420
Cledb Astudillo <cledb.uxcodes@gmail.com>
2021-12-16 07:30:52 +0000

    Status Codes

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.get('/restaurants/:id', function (req, res) {
         }
     }
 
-    res.render('404');
+    res.status(404).render('404');
 });
 
 app.get('/about', function (req, res) {
@@ -78,11 +78,11 @@ app.get('/confirm', function (req, res) {
 });
 
 app.use(function (req, res) {
-    res.render('404');
+    res.status(404).render('404');
 });
 
 app.use(function (error, req, res, next) {
-    res.render('500');
+    res.status(500).render('500');
 });
 
 app.listen(3000);
